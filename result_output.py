bd3d68aabc4445f173b9a863819aedbbe9d55c9e
MarsPain <1434579002@qq.com>
2018-08-10 16:18:55 +0800

    将识别出的新实体、测试集真实的实体、新实体中正确的实体根据不同的实体类别输出到CSV中

--- result_output.py
+++ result_output.py
@@ -5,7 +5,7 @@ path_data_all = "data/data_all_fangji.csv"  # 原方剂数据集
 path_ner_result = "result/ner_predict_test.utf8"    # 命名实体识别结果
 path_result = "result/result.csv"   # 存储被识别出来的实体的标准表示
 path_data_all_ner = "result/data_all_ner.csv"   # 将命名实体识别结果写到原方剂数据集中
-path_entity_new = "result/entity_new.txt"  # 存储被识别出来的原词库中不存在的实体（后期进行人工审核，统计其中被发现的新词）
+path_new_true_entity = "result/new_true_entity.csv"  # 存储测试集中被识别出的新实体、测试集真实的实体、新实体中正确的实体
 
 
 def get_data():
@@ -76,7 +76,7 @@ def find_new_entity():
     :return:
     """
     sets_name_list = ["diseases", "pattern", "treat", "symptom"]    # 四个原实体词库的文件名（病名、证型、治疗手段、症状）
-    set_old_list = [set(), set(), set(), set()]  # 用于保存原实体词库中的不同实体的集合列表
+    set_old_list = [set(), set(), set(), set()]  # 用于保存训练集的实体词库中的不同实体的集合列表
     for index, set_name in enumerate(sets_name_list):
         path_set = os.path.join("data", set_name+"_7000.txt")
         with open(path_set, "r", encoding="utf-8") as f_set:
@@ -112,10 +112,56 @@ def find_new_entity():
     set_new_treat = set_ner_treat - set_old_treat
     set_new_symptom = set_ner_symptom - set_old_symptom
     print(set_new_symptom)
-    # with open(path_entity_new, "w", encoding="utf-8") as f_new:  # 将新实体写入txt文件
-    #     list_new = list(set_new)
-    #     entity_string = "\n".join(list_new)
-    #     f_new.write(entity_string)
+    set_new_diseases_series = pd.Series(list(set_new_diseases), name="new_diseases")
+    set_new_pattern_series = pd.Series(list(set_new_pattern), name="new_pattern")
+    set_new_treat_series = pd.Series(list(set_new_treat), name="new_treat")
+    set_new_symptom_series = pd.Series(list(set_new_symptom), name="new_symptom")
+    print(set_new_symptom_series)
+    set_true_list = [set(), set(), set(), set()]  # 用于保存测试集中不同实体的集合列表
+    for index, set_name in enumerate(sets_name_list):
+        path_set = os.path.join("data", set_name+"_4000.txt")
+        with open(path_set, "r", encoding="utf-8") as f_set:
+            lines = f_set.readlines()
+            for line in lines:
+                entity = line.strip()
+                set_true_list[index].add(entity)
+    # 用于保存测试集中不同实体的四个集合
+    set_true_diseases = set_true_list[0]
+    set_true_pattern = set_true_list[1]
+    set_true_treat = set_true_list[2]
+    set_true_symptom = set_true_list[3]
+    set_true_diseases_series = pd.Series(list(set_true_diseases), name="true_diseases")
+    set_true_pattern_series = pd.Series(list(set_true_pattern), name="true_treat")
+    set_true_treat_series = pd.Series(list(set_true_treat), name="true_treat")
+    set_true_symptom_series = pd.Series(list(set_true_symptom), name="true_symptom")
+    # 得到被发现的新实体集合与测试集的实体集合之间的交集，则为被发现的新实体中正确的实体
+    set_new_true_diseases = set_new_diseases & set_true_diseases
+    set_new_true_pattern = set_new_pattern & set_true_pattern
+    set_new_true_treat = set_new_treat & set_true_treat
+    set_new_true_symptom = set_new_symptom & set_true_symptom
+    print(set_new_true_symptom)
+    set_new_true_diseases_series = pd.Series(list(set_new_true_diseases), name="new_true_diseases")
+    set_new_true_pattern_series = pd.Series(list(set_new_true_pattern), name="new_true_pattern")
+    set_new_true_treat_series = pd.Series(list(set_new_true_treat), name="new_true_treat")
+    set_new_true_symptom_series = pd.Series(list(set_new_true_symptom), name="new_true_symptom")
+    # 通过拼接Series创建最终DataFrame的方法
+    new_true_entity_list = [set_new_diseases_series, set_true_diseases_series, set_new_true_diseases_series,
+                            set_new_pattern_series, set_true_pattern_series, set_new_true_pattern_series,
+                            set_new_treat_series, set_true_treat_series, set_new_true_treat_series,
+                            set_new_symptom_series, set_true_symptom_series, set_new_true_symptom_series]
+    new_true_entity = pd.concat(new_true_entity_list, axis=1)
+    new_true_entity.to_csv(path_new_true_entity, encoding="utf-8")
+    # 直接写成一个DataFrame的方法(遗留了列数不匹配的问题)
+    # new_true_entity_list = [list(set_new_diseases), list(set_true_diseases), list(set_new_true_diseases),
+    #                         list(set_new_pattern), list(set_true_pattern), list(set_new_true_pattern),
+    #                         list(set_new_treat), list(set_true_treat), list(set_new_true_treat),
+    #                         list(set_new_symptom), list(set_true_symptom), list(set_new_true_symptom)]
+    # new_true_entity_name_list = ["new_diseases", "true_diseases", "new_true_diseases",
+    #                              "new_pattern", "true_pattern", "new_true_pattern",
+    #                              "new_treat", "true_treat", "new_true_treat",
+    #                              "new_symptom", "true_symptom", "new_true_symptom"]
+    # new_true_entity = pd.DataFrame(new_true_entity_list, columns=new_true_entity_name_list)
+    # new_true_entity.to_csv(path_new_true_entity, encoding="utf-8")
 
 
 def write_to_data():
@@ -140,6 +186,6 @@ def write_to_data():
 
 
 if __name__ == "__main__":
-    # get_data()
+    get_data()
     find_new_entity()
     # write_to_data()
